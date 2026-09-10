2f5d123d9ba06153823def6f44b3f5ff89183023
Michael Major <IAMSIMPLYMAJOR@GMAIL.COM>
2026-09-09 19:21:47 -0500

    test(e2e): keep pilot matrix draft unexpired

--- e2e/customer-centered-workspace.spec.js
+++ e2e/customer-centered-workspace.spec.js
@@ -77,6 +77,7 @@ async function seedPilotQuote(page) {
       latestVersionNumber: 1,
       createdAtISO: "2026-08-10T14:00:00.000Z",
       updatedAtISO: "2026-08-10T15:00:00.000Z",
+      expiresAtISO: "2099-08-24T14:00:00.000Z",
       customer: {
         name: "Pilot Customer",
         email: "pilot@example.test",
